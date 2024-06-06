a25ea37590c8e60e37ef01e8154704393e59f3ad
Juan David <jciro1360@gmail.com>
2024-06-05 20:34:10 -0500

    page de cursos implementacion back

--- src/routes/courses/courses.routes.js
+++ src/routes/courses/courses.routes.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import upload from '../../helpers/upload.js';
 
 
-import { createCourse, getAllCourses, getCourse , updateCourse, deleteCourse} from "../../controllers/courses/course.controller.js";
+import { createCourse, getAllCourses, getCourse , updateCourse, deleteCourse, getCoursesByCategory} from "../../controllers/courses/course.controller.js";
 
 const router = Router();
 
 router.get('/getAllCourses', getAllCourses);
 router.get('/getCourse/:id', getCourse);
+router.get('/category/:categoryName', getCoursesByCategory);
+
 router.post('/createCourse', upload.single('image'), createCourse);
 router.put('/updateCourse/:id', updateCourse);
 router.delete('/deleteCourse/:id', deleteCourse);
