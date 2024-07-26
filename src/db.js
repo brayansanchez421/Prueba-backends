31cf25542d496cc122af8728508dfe5c9a6a222f
brayansanchez421 <78933555+brayansanchez421@users.noreply.github.com>
2024-07-26 14:07:10 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,12 +1,35 @@
 import mongoose from "mongoose";
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 export const connectDB = async () => {
-    const mongoURI = "mongodb://mongo:UeIGycJgqljTDECTDmGuLnqhQacfYFKl@viaduct.proxy.rlwy.net:33294";
+    const mongoURI = "mongodb+srv://sanchezcortesbrayan:Wlcr9dz6Ql1SvmmS@brightmind.bkeumrn.mongodb.net/plataforma?retryWrites=true&w=majority&appName=brightmind";
 
     try {
         await mongoose.connect(mongoURI);
         console.log("se ha conectado JAJAJAJa");
+
+        // Verifica la conexión a la base de datos en la ruta
+        app.get('/status', async (req, res) => {
+            try {
+                await mongoose.connection.db.command({ ping: 1 });
+                res.status(200).send('Conexión a la base de datos exitosa');
+            } catch (error) {
+                res.status(500).send('Error al conectar con la base de datos');
+            }
+        });
     } catch (error) {
         console.log(error);
+        // Maneja el error de conexión si es necesario
     }
 };
+
+// Inicia el servidor Express
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
+
+// Conecta a la base de datos
+connectDB();
