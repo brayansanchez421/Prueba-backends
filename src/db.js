f689769649576df838272ad120eeba32445f6323
brayansanchez421 <78933555+brayansanchez421@users.noreply.github.com>
2024-07-26 13:49:43 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
+    const mongoURI = "mongodb://mongo:UeIGycJgqljTDECTDmGuLnqhQacfYFKl@viaduct.proxy.rlwy.net:33294";
+
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("se ha conectado JAJAJAJa")
+        await mongoose.connect(mongoURI);
+        console.log("se ha conectado JAJAJAJa");
     } catch (error) {
         console.log(error);
     }
 };
-
