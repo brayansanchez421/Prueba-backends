2efb314a188b7e83737f811843f6505d81d8509a
Brayansanchez10 <115506920+Brayansanchez10@users.noreply.github.com>
2024-07-29 14:45:45 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,36 @@
 import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import RoutesApp from "./routes/index.js";
 import { connectDB } from "./db.js";
 import cors from 'cors';
 import morgan from "morgan";
 
+dotenv.config();
 
 const app = express();
 
-dotenv.config();
+// Conectar a MongoDB
+connectDB();
 
+// Configuración de CORS
+const corsOptions = {
+  origin: 'https://brightmind3.netlify.app', // Permitir solo este origen
+  credentials: true, // Permitir enviar cookies de autenticación
+  optionsSuccessStatus: 200 // Algunos navegadores (IE11, algunos SmartTVs) no soportan 204
+};
 
-// ToDo: app.on()
-app.use(cors({
-    origin: 'https://brightmind3.netlify.app', 
-    credentials: true
+app.use(cors(corsOptions)); // Registrar el middleware de CORS
 
+// Configuración de express-session
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'tu_secreto', // Cambia esto por un secreto único
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }), // Almacenar sesiones en MongoDB
+  cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 } // Ajusta según tus necesidades
 }));
 
 app.use(morgan("dev"));
@@ -24,6 +38,10 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/PE", RoutesApp);
 
-
+// Middleware de manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal!');
+});
 
 export default app;
