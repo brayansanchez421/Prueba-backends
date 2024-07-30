56025ed08875a65619e71c7e4377dba55842d0dc
Brayansanchez10 <115506920+Brayansanchez10@users.noreply.github.com>
2024-07-30 14:51:47 -0500

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,21 +1,23 @@
-import  Jwt  from "jsonwebtoken";
+import Jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
 
-export const authRequired = (req, res, next)=>{
-   try {
-    const {token} = req.cookies;
+export const authRequired = (req, res, next) => {
+  try {
+    const { token } = req.cookies;
 
-    if(!token)
-    return res.status(401).json({message: "authorization denied"});
-    Jwt.verify(token, TOKEN_SECRET,(err, user)=>{
-        if(err) return res.status(403).json({message: "invalid token"});
-        req.user = user
-    } )
-    next();
+    if (!token) {
+      return res.status(401).json({ message: "authorization denied" });
+    }
+
+    Jwt.verify(token, TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: "invalid token" });
+      }
+      req.user = user;
+      next();
+    });
     
-   } catch (error) {
+  } catch (error) {
     return res.status(500).json({ message: error.message });
-
-   }
-    
-}
+  }
+};
